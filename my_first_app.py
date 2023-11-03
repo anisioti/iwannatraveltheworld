fe073fcc5863bd7321e02acfceca578985e58b32
Athina Nisioti <athina.nisioti97@gmail.com>
2023-11-03 13:16:51 +0100

    first app attempt

---
+++ my_first_app.py
@@ -0,0 +1,15 @@
+import streamlit as st
+import streamlit.components.v1 as components
+#st.markdown("# I wanna travel the World🎈")
+#st.sidebar.markdown("# Main page 🎈")
+
+
+
+st.set_page_config(page_title="I wanna travel the World", page_icon="🌍", layout="centered", initial_sidebar_state="collapsed")
+
+
+st.title("I wanna travel the World 🌍✈️")
+st.subheader("Mind traveling to any place in the earth you want!")
+
+
+#components.html(html=stripe_js, height=300)
