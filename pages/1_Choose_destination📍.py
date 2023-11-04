e4edc9b52bee886c1156b4406772788f3505ce83
Athina Nisioti <athina.nisioti97@gmail.com>
2023-11-04 18:56:41 +0100

    small change in the message of second page

--- pages/1_Choose_destination📍.py
+++ pages/1_Choose_destination📍.py
@@ -4,7 +4,7 @@ import streamlit.components.v1 as components
 st.title("Where do you want to travel today?📍")
 st.subheader("Choose your destination wisely-it can affect your mood!")
 
-user_input = st.text_input("Enter a city:", "")
+user_input = st.text_input("Enter a destination:", "")
 
 # Display the user's input
-st.write("You entered:", user_input)
+st.write("Information about your destination:", user_input)
